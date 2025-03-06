5f17277cb3339f69531d181d4200c681558b147b
Hem1234567 <hem07042006@gmail.com>
2025-03-06 13:49:20 +0530

    js file added

---
+++ javascript/day-7/index.js
@@ -0,0 +1,155 @@
+/////////////////////////
+
+// variables in js//
+
+// var num = "10";
+// console.log(num);
+
+// let name = "hem";
+// console.log(name);
+
+// const team = "india";
+// console.log(team);
+
+
+// let result = 10 + 20;
+// console.log(result);
+
+
+///////////////////////////
+
+
+//variablesdifference in js//
+
+
+//declaration and initialization of variable
+
+// var num = 10;
+// console.log(num);
+
+//re-declaration of var
+
+// var num = 20;
+// console.log(num);
+
+// undefined 
+// var num = 20;
+// console.log(num);
+
+// var num;
+// console.log(num);
+
+
+
+
+//declaration of let variables
+
+// let num1 = 10;
+// console.log(num1);
+
+// num1 = 20;
+// console.log(num1);
+
+
+//const variables and declaration of const variable  
+
+// const num = 10 ;
+// console.log(num);
+
+// const num = 10.5 ;
+// console.log(num);
+// console.log(typeof str);
+
+// let str = 'hello';
+// console.log(str);
+// console.log(typeof(str));
+
+// let ispendingresult = true;
+// console.log(ispendingresult);
+// console.log(typeof ispendingresult);
+
+// let empty = null;
+// console.log(empty);
+// console.log(typeof(empty));
+
+// let name;
+// console.log(name);
+// console.log(typeof(name));
+
+//arithmatic operators//
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log("addition =",num1+num2);
+
+// let num3 = 20;
+// let num4 = 10;
+// console.log("subtraction =",num3 - num4);
+
+// let num5 = 5;
+// console.log("multiplication =",num5 * 10);
+
+// let num6 = 20;
+// let divresult = num6 / 2;
+// console.log(divresult);
+
+// let num7 = 20;
+// let moduleresult = num6 / 2;
+// console.log(moduleresultresult);
+
+
+
+//comparision operator//
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1>num2);
+// console.log(num1<num2);
+
+// console.log(10>=10);
+// console.log(10 <= 9);
+
+// let str = "10";
+// let num = "10";
+
+// console.log(str == num);
+
+// console.log(str === num);
+// console.log(10 != 10);
+// console.log(5 != 10);
+
+
+//logical operators//
+// let num1 = 10;
+// let num2 = 20;
+// if (num1 > 2 && num2 > 10) {
+//     console.log("20 is greater than 10");
+// } 
+
+
+//or operator//
+
+let num1 = 10;
+let num2 = 20;
+
+if (num3 > 5 || num4 > 50) {
+    console.log("condition is true or satisfied");
+} else {
+    console.log("condition is false or not satisfied");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
